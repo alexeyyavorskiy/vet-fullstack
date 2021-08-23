bfcd7b1c492a59b8b353d00e82e7775568f1c250
alexeyyavorskiy <yavorskiy.alexey@gmail.com>
2021-08-24 00:50:57 +0300

    Added settings env file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,43 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { AnimalsModule } from './modules/animals/animals.module';
-import { OwnersModule } from './modules/owners/owners.module';
-import { Owner } from './modules/owners/owners.model';
-import { Species } from './modules/specieses/species.model';
-import { Address } from './modules/addresses/adresses.model';
-import { AuthModule } from './modules/auth/auth.module';
-import { User } from './modules/users/users/users.model';
-import { UsersModule } from './modules/users/users/users.module';
-import { Animal } from './modules/animals/animals.model';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import {Module} from '@nestjs/common';
+import {SequelizeModule} from '@nestjs/sequelize';
+import {AnimalsModule} from './modules/animals/animals.module';
+import {OwnersModule} from './modules/owners/owners.module';
+import {Owner} from './modules/owners/owners.model';
+import {Species} from './modules/specieses/species.model';
+import {Address} from './modules/addresses/adresses.model';
+import {AuthModule} from './modules/auth/auth.module';
+import {User} from './modules/users/users/users.model';
+import {UsersModule} from './modules/users/users/users.module';
+import {Animal} from './modules/animals/animals.model';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  controllers: [],
-  providers: [],
-  imports: [
-    AnimalsModule,
-    OwnersModule,
-    AuthModule,
-    UsersModule,
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'vet-base',
-      models: [Species, Address, Owner, Animal, User],
-      autoLoadModels: true,
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'build'),
-    }),
-  ],
+    controllers: [],
+    providers: [],
+    imports: [
+        AnimalsModule,
+        OwnersModule,
+        AuthModule,
+        UsersModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: 5432,
+            username: process.env.POSTGRES_USERNAME,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+            models: [Species, Address, Owner, Animal, User],
+            autoLoadModels: true,
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'build'),
+        }),
+    ],
 })
 export class AppModule {
 }
